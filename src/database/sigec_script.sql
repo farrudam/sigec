2fb1aff70c916bdd5237eb664910d92683a3be6b
Fábio Arruda Magalhães <fabioarrudamagalhaes@gmail.com>
2023-04-26 16:58:12 -0300

    new update script database

--- src/database/sigec_script.sql
+++ src/database/sigec_script.sql
@@ -7,7 +7,7 @@ USE `sigec`;
 
 CREATE TABLE `bloco` (
     `id` int NOT NULL AUTO_INCREMENT,
-    `nome` varchar(100) NOT NULL,
+    `nome` varchar(80) NOT NULL,
     PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -15,34 +15,35 @@ CREATE TABLE `bloco` (
 CREATE TABLE `sala` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_bloco` int NOT NULL,
-  `nome` varchar(100) NOT NULL,
+  `nome` varchar(80) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `chave`(
     `id` int NOT NULL AUTO_INCREMENT,    
     `id_sala` int not null,
-    `etiqueta` varchar(20) unique not null,
-    `descricao` varchar(255) not null,    
+    `etiqueta` varchar(30) unique not null,
+    `descricao` varchar(80) not null,    
     `situacao` ENUM('disponivel', 'emprestada', 'manutenção') DEFAULT 'disponivel',
-    `habilitada` boolean DEFAULT true,
+    `habilitada` tinyint(1) DEFAULT '1',
+    `restrita` tinyint(1) DEFAULT '0',
     PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE `usuario` (  
   `id` int NOT NULL AUTO_INCREMENT,
-  `matricula` int NOT NULL,
+  `matricula` int NOT NULL UNIQUE,
   `nome` varchar(255) NOT NULL,
   `senha` varchar(255) DEFAULT NULL,
-  `telefone` varchar(50) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `celular` varchar(50) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `url_foto` varchar(255) DEFAULT NULL,
   `habilitado` tinyint(1) DEFAULT '1',
   `doc_autorizacao` varchar(255) DEFAULT NULL,
   `tipo` enum('aluno','servidor', 'terceirizado') NOT NULL,
-  `permissao` enum('administrador', 'portaria', 'solicitante') NOT NULL DEFAULT 'solicitante',
-  PRIMARY KEY (`id`,`matricula`)
+  `perfil` enum('administrador', 'portaria', 'solicitante') NOT NULL DEFAULT 'solicitante',
+  PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -56,22 +57,21 @@ CREATE TABLE `restricao_chave` (
 
 CREATE TABLE `emprestimo` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `mat_admin` int NOT NULL,
+  `mat_admin_abertura` int NOT NULL,
+  `mat_admin_encerramento` int NOT NULL,
   `mat_solic` int NOT NULL,
   `data_emprestimo` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `data_devolucao` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `observacao` varchar(100) DEFAULT NULL,
   `situacao` enum('ativo','encerrado') NOT NULL DEFAULT 'ativo',
-    PRIMARY KEY (`id`)
-    
+    PRIMARY KEY (`id`)    
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE `item_emprestimo` (    
   `id_emprestimo` int NOT NULL,
-  `id_chave` int NOT NULL,
-  `retirada_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `devolvida_em` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `id_chave` int NOT NULL,  
+  `devolvido_em` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id_emprestimo`, `id_chave`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
